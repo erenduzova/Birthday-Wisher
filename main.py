2bcf18163303ef36ff0a595d9c4835d00f62af3b
Eren Düzova <erenduzova@gmail.com>
2022-09-07 11:12:40 +0300

    datetime module integrated.

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import smtplib
+import datetime as dt
+import random
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("PASSWORD")
+TO_ADR = os.getenv("TO_ADR")
 
-my_email = os.getenv("MY_EMAIL")
-password = os.getenv("PASSWORD")
-to_adr = os.getenv("TO_ADR")
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs=to_adr,
-                        msg="Subject:Hello\n\nThis is the body of my email")
+def select_quote():
+    with open("quotes.txt") as quotes_file:
+        all_quotes = quotes_file.readlines()
+    return random.choice(all_quotes)
+
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    quote = select_quote()
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=TO_ADR,
+                            msg=f"Subject:Monday Motivation\n\n{quote}")
+else:
+    print("No")
+
